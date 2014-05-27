012328fd581922ad458d480cb3229d39061cfbfc
Madhukar <madhukar@confluxtechnologies.com>
2014-05-27 21:41:41 +0530

    Add support of Tranches

--- src/main/java/org/openmf/mifos/dataimport/dto/loan/GroupLoan.java
+++ src/main/java/org/openmf/mifos/dataimport/dto/loan/GroupLoan.java
@@ -1,27 +1,32 @@
 package org.openmf.mifos.dataimport.dto.loan;
 
-public class GroupLoan extends Loan{
-	
-	private final String groupId;
-	private final String externalId;
-	public GroupLoan(String loanType, String groupId, String productId,	String loanOfficerId, String submittedOnDate, String fundId,
-			String principal, String numberOfRepayments, String repaymentEvery,	String repaymentFrequencyType, String loanTermFrequency,
-			String loanTermFrequencyType, String interestRatePerPeriod,	String expectedDisbursementDate, String amortizationType,
-			String interestType, String interestCalculationPeriodType, String inArrearsTolerance, String transactionProcessingStrategyId,
-			String graceOnPrincipalPayment, String graceOnInterestPayment, String graceOnInterestCharged, String interestChargedFromDate,
-			String repaymentsStartingFromDate, Integer rowIndex, String status,String externalId) {
-		
-		super(loanType, null, productId, loanOfficerId, submittedOnDate, fundId, principal, numberOfRepayments, repaymentEvery, repaymentFrequencyType,
-				loanTermFrequency, loanTermFrequencyType, interestRatePerPeriod, expectedDisbursementDate, amortizationType, interestType,
-				interestCalculationPeriodType, inArrearsTolerance, transactionProcessingStrategyId, graceOnPrincipalPayment,
-				graceOnInterestPayment, graceOnInterestCharged,	interestChargedFromDate, repaymentsStartingFromDate, rowIndex, status, externalId);
-		
-		this.groupId = groupId;
-		this.externalId=externalId;
-	   }
+import java.util.List;
 
-	public String getGroupId() {
-		return groupId;
-	}
+public class GroupLoan extends Loan {
+
+    private final String groupId;
+
+    public GroupLoan(final String loanType, final String groupId, final String productId, final String loanOfficerId,
+            final String submittedOnDate, final String fundId, final String principal, final String numberOfRepayments,
+            final String repaymentEvery, final String repaymentFrequencyType, final String loanTermFrequency,
+            final String loanTermFrequencyType, final String interestRatePerPeriod, final String expectedDisbursementDate,
+            final String amortizationType, final String interestType, final String interestCalculationPeriodType,
+            final String inArrearsTolerance, final String transactionProcessingStrategyId, final String graceOnPrincipalPayment,
+            final String graceOnInterestPayment, final String graceOnInterestCharged, final String interestChargedFromDate,
+            final String repaymentsStartingFromDate, final Integer rowIndex, final String status, final String externalId,
+            final String fixedEmiAmount, final String maxOutstandingLoanBalance, final List<EmiDetail> disbursementData) {
+
+        super(loanType, null, productId, loanOfficerId, submittedOnDate, fundId, principal, numberOfRepayments, repaymentEvery,
+                repaymentFrequencyType, loanTermFrequency, loanTermFrequencyType, interestRatePerPeriod, expectedDisbursementDate,
+                amortizationType, interestType, interestCalculationPeriodType, inArrearsTolerance, transactionProcessingStrategyId,
+                graceOnPrincipalPayment, graceOnInterestPayment, graceOnInterestCharged, interestChargedFromDate,
+                repaymentsStartingFromDate, rowIndex, status, externalId, fixedEmiAmount, maxOutstandingLoanBalance, disbursementData);
+
+        this.groupId = groupId;
+    }
+
+    public String getGroupId() {
+        return this.groupId;
+    }
 
 }
--- src/main/java/org/openmf/mifos/dataimport/dto/loan/Loan.java
+++ src/main/java/org/openmf/mifos/dataimport/dto/loan/Loan.java
@@ -1,218 +1,242 @@
 package org.openmf.mifos.dataimport.dto.loan;
 
+import java.util.List;
 import java.util.Locale;
 
 public class Loan {
 
-	private final transient Integer rowIndex;
-	
-	private final transient String status;
-	
-	private final String clientId;
-	
-	private final String productId;
-	
+    private final transient Integer rowIndex;
+
+    private final transient String status;
+
+    private final String clientId;
+
+    private final String productId;
+
     private final String externalId;
-	
-	private final String loanOfficerId;
-	
-	private final String fundId;
-	
-	private final String submittedOnDate;
-	
-	private final String principal;
-	
+
+    private final String loanOfficerId;
+
+    private final String fundId;
+
+    private final String submittedOnDate;
+
+    private final String principal;
+
     private final String numberOfRepayments;
-	
-	private final String repaymentEvery;
-	
-	private final String repaymentFrequencyType;
-	
+
+    private final String repaymentEvery;
+
+    private final String repaymentFrequencyType;
+
     private final String loanTermFrequency;
-	
-	private final String loanTermFrequencyType;
-	
-	private final String interestRatePerPeriod;
-	
-	private final String expectedDisbursementDate;
-	
-	private final String amortizationType;
-	
-	private final String interestType;
-	
-	private final String interestCalculationPeriodType;
-	
-	private final String inArrearsTolerance;
-	
-	private final String transactionProcessingStrategyId;
-	
-	private final String graceOnInterestCharged;
-	
-	private final String graceOnInterestPayment;
-	
-	private final String graceOnPrincipalPayment;
-	
-	private final String interestChargedFromDate;
-	
-	private final String repaymentsStartingFromDate;
-	
-	private final String dateFormat;
-	
-	private final Locale locale;
-	
-	private final String loanType;
-	
-	public Loan(String loanType, String clientId, String productId, String loanOfficerId, String submittedOnDate, String fundId, String principal, String numberOfRepayments, String repaymentEvery,
-			String repaymentFrequencyType,  String loanTermFrequency, String loanTermFrequencyType, String interestRatePerPeriod, String expectedDisbursementDate, String amortizationType,
-			String interestType, String interestCalculationPeriodType, String inArrearsTolerance, String transactionProcessingStrategyId, String graceOnPrincipalPayment,
-			String graceOnInterestPayment, String graceOnInterestCharged, String interestChargedFromDate, String repaymentsStartingFromDate, Integer rowIndex, String status,String externalId) {
-		this.amortizationType = amortizationType;
-		this.clientId = clientId;
-		this.expectedDisbursementDate = expectedDisbursementDate;
-		this.fundId = fundId;
-		this.externalId= externalId;
-		this.graceOnInterestCharged = graceOnInterestCharged;
-		this.graceOnInterestPayment = graceOnInterestPayment;
-		this.graceOnPrincipalPayment = graceOnPrincipalPayment;
-		this.inArrearsTolerance = inArrearsTolerance;
-		this.interestCalculationPeriodType = interestCalculationPeriodType;
-		this.interestChargedFromDate = interestChargedFromDate;
-		this.interestRatePerPeriod = interestRatePerPeriod;
-		this.interestType = interestType;
-		this.loanOfficerId = loanOfficerId;
-		this.loanTermFrequency = loanTermFrequency;
-		this.loanTermFrequencyType = loanTermFrequencyType;
-		this.numberOfRepayments = numberOfRepayments;
-		this.principal = principal;
-		this.productId = productId;
-		this.repaymentEvery = repaymentEvery;
-		this.repaymentFrequencyType = repaymentFrequencyType;
-		this.repaymentsStartingFromDate = repaymentsStartingFromDate;
-		this.submittedOnDate = submittedOnDate;
-		this.transactionProcessingStrategyId = transactionProcessingStrategyId;
-		this.dateFormat = "dd MMMM yyyy";
-		this.locale = Locale.ENGLISH;
-		this.loanType = loanType;
-		this.rowIndex = rowIndex;
-		this.status = status;
-	}
-	
-	public String getAmortizationType() {
-		return amortizationType;
-	}
-	
-	public String getClientId() {
-		return clientId;
-	}
-	
-	public String getExpectedDisbursementDate() {
-		return expectedDisbursementDate;
-	}
-	
-	public String getFundId() {
-		return fundId;
-	}
-	
-	public String getGraceOnInterestCharged() {
-		return graceOnInterestCharged;
-	}
-	
-	public String getGraceOnInterestPayment() {
-		return graceOnInterestPayment;
-	}
-	
-	public String getGraceOnPrincipalPayment() {
-		return graceOnPrincipalPayment;
-	}
-	
-	public String getInArrearsTolerance() {
-		return inArrearsTolerance;
-	}
-	
-	public String getInterestCalculationPeriodType() {
-		return interestCalculationPeriodType;
-	}
-	
-	public String getInterestChargedFromDate() {
-		return interestChargedFromDate;
-	}
-	
-	public String getInterestRatePerPeriod() {
-		return interestRatePerPeriod;
-	}
-	
-	public String getInterestType() {
-		return interestType;
-	}
-	
-	public String getLoanOfficerId() {
-		return loanOfficerId;
-	}
-	
-	public String getLoanTermFrequencyType() {
-		return loanTermFrequencyType;
-	}
-	
-	public String getLoanTermFrequency() {
-		return loanTermFrequency;
-	}
-	
-	public String getLoanType() {
-		return loanType;
-	}
-	
-	public String getNumberOfRepayments() {
-		return numberOfRepayments;
-	}
-	
-	public String getPrincipal() {
-		return principal;
-	}
-	
-	public String getProductId() {
-		return productId;
-	}
-	
-	public String getRepaymentEvery() {
-		return repaymentEvery;
-	}
-	
-	public String getRepaymentFrequencyType() {
-		return repaymentFrequencyType;
-	}
-	
-	public String getRepaymentsStartingFromDate() {
-		return repaymentsStartingFromDate;
-	}
-	
-	public String getSubmittedOnDate() {
-		return submittedOnDate;
-	}
-	
-	public String getTransactionProcessingStrategyId() {
-		return transactionProcessingStrategyId;
-	}
-	
-	public String getDateFormat() {
-		return dateFormat;
-	}
-	
-	public Locale getLocale() {
-		return locale;
-	}
-	
-	public Integer getRowIndex() {
+
+    private final String loanTermFrequencyType;
+
+    private final String interestRatePerPeriod;
+
+    private final String expectedDisbursementDate;
+
+    private final String amortizationType;
+
+    private final String interestType;
+
+    private final String interestCalculationPeriodType;
+
+    private final String inArrearsTolerance;
+
+    private final String transactionProcessingStrategyId;
+
+    private final String graceOnInterestCharged;
+
+    private final String graceOnInterestPayment;
+
+    private final String graceOnPrincipalPayment;
+
+    private final String interestChargedFromDate;
+
+    private final String repaymentsStartingFromDate;
+
+    private final String dateFormat;
+
+    private final Locale locale;
+
+    private final String loanType;
+
+    private final String fixedEmiAmount;
+
+    private final String maxOutstandingLoanBalance;
+
+    private final List<EmiDetail> disbursementData;
+
+    public Loan(String loanType, String clientId, String productId, String loanOfficerId, String submittedOnDate, String fundId,
+            String principal, String numberOfRepayments, String repaymentEvery, String repaymentFrequencyType, String loanTermFrequency,
+            String loanTermFrequencyType, String interestRatePerPeriod, String expectedDisbursementDate, String amortizationType,
+            String interestType, String interestCalculationPeriodType, String inArrearsTolerance, String transactionProcessingStrategyId,
+            String graceOnPrincipalPayment, String graceOnInterestPayment, String graceOnInterestCharged, String interestChargedFromDate,
+            String repaymentsStartingFromDate, Integer rowIndex, String status, String externalId, String fixedEmiAmount,
+            String maxOutstandingLoanBalance, List<EmiDetail> disbursementData) {
+        this.amortizationType = amortizationType;
+        this.clientId = clientId;
+        this.expectedDisbursementDate = expectedDisbursementDate;
+        this.fundId = fundId;
+        this.externalId = externalId;
+        this.graceOnInterestCharged = graceOnInterestCharged;
+        this.graceOnInterestPayment = graceOnInterestPayment;
+        this.graceOnPrincipalPayment = graceOnPrincipalPayment;
+        this.inArrearsTolerance = inArrearsTolerance;
+        this.interestCalculationPeriodType = interestCalculationPeriodType;
+        this.interestChargedFromDate = interestChargedFromDate;
+        this.interestRatePerPeriod = interestRatePerPeriod;
+        this.interestType = interestType;
+        this.loanOfficerId = loanOfficerId;
+        this.loanTermFrequency = loanTermFrequency;
+        this.loanTermFrequencyType = loanTermFrequencyType;
+        this.numberOfRepayments = numberOfRepayments;
+        this.principal = principal;
+        this.productId = productId;
+        this.repaymentEvery = repaymentEvery;
+        this.repaymentFrequencyType = repaymentFrequencyType;
+        this.repaymentsStartingFromDate = repaymentsStartingFromDate;
+        this.submittedOnDate = submittedOnDate;
+        this.transactionProcessingStrategyId = transactionProcessingStrategyId;
+        this.dateFormat = "dd MMMM yyyy";
+        this.locale = Locale.ENGLISH;
+        this.loanType = loanType;
+        this.rowIndex = rowIndex;
+        this.status = status;
+        this.fixedEmiAmount = fixedEmiAmount;
+        this.maxOutstandingLoanBalance = maxOutstandingLoanBalance;
+        this.disbursementData = disbursementData;
+    }
+
+    public String getAmortizationType() {
+        return amortizationType;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getExpectedDisbursementDate() {
+        return expectedDisbursementDate;
+    }
+
+    public String getFundId() {
+        return fundId;
+    }
+
+    public String getGraceOnInterestCharged() {
+        return graceOnInterestCharged;
+    }
+
+    public String getGraceOnInterestPayment() {
+        return graceOnInterestPayment;
+    }
+
+    public String getGraceOnPrincipalPayment() {
+        return graceOnPrincipalPayment;
+    }
+
+    public String getInArrearsTolerance() {
+        return inArrearsTolerance;
+    }
+
+    public String getInterestCalculationPeriodType() {
+        return interestCalculationPeriodType;
+    }
+
+    public String getInterestChargedFromDate() {
+        return interestChargedFromDate;
+    }
+
+    public String getInterestRatePerPeriod() {
+        return interestRatePerPeriod;
+    }
+
+    public String getInterestType() {
+        return interestType;
+    }
+
+    public String getLoanOfficerId() {
+        return loanOfficerId;
+    }
+
+    public String getLoanTermFrequencyType() {
+        return loanTermFrequencyType;
+    }
+
+    public String getLoanTermFrequency() {
+        return loanTermFrequency;
+    }
+
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public String getNumberOfRepayments() {
+        return numberOfRepayments;
+    }
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getRepaymentEvery() {
+        return repaymentEvery;
+    }
+
+    public String getRepaymentFrequencyType() {
+        return repaymentFrequencyType;
+    }
+
+    public String getRepaymentsStartingFromDate() {
+        return repaymentsStartingFromDate;
+    }
+
+    public String getSubmittedOnDate() {
+        return submittedOnDate;
+    }
+
+    public String getTransactionProcessingStrategyId() {
+        return transactionProcessingStrategyId;
+    }
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public Integer getRowIndex() {
         return rowIndex;
     }
-	
-	public String getStatus() {
+
+    public String getStatus() {
         return status;
     }
 
-	public String getExternalId() {
-		return externalId;
-	
-	}
+    public String getExternalId() {
+        return externalId;
 
-	
+    }
+
+    public String getFixedEmiAmount() {
+        return this.fixedEmiAmount;
+    }
+
+    public String getMaxOutstandingLoanBalance() {
+        return this.maxOutstandingLoanBalance;
+    }
+
+    public List<EmiDetail> getDisbursementData() {
+        return this.disbursementData;
+    }
+
 }
